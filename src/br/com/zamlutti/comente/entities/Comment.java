b49b1ea4111383e72534008ce75bc4d30f139b30
Karen Zamlutti <zamlutti@gmail.com>
2013-05-27 22:59:29 -0300

    Map comment and entry relations

--- src/br/com/zamlutti/comente/entities/Comment.java
+++ src/br/com/zamlutti/comente/entities/Comment.java
@@ -1,36 +1,49 @@
 package br.com.zamlutti.comente.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
-    @Id
-    @GeneratedValue
-    private int id;
-    private String email;
-    private String text;
+	@Id
+	@GeneratedValue
+	private int id;
+	private String email;
+	private String text;
+	@ManyToOne(cascade = { CascadeType.ALL })
+	private Entry entry;
 
-    public int getId() {
-        return this.id;
-    }
+	public int getId() {
+		return this.id;
+	}
 
-    public String getEmail() {
-        return this.email;
-    }
+	public String getEmail() {
+		return this.email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getText() {
-        return this.text;
-    }
+	public String getText() {
+		return this.text;
+	}
 
-    public void setText(String text) {
-        this.text = text;
-    }
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Entry getEntry() {
+		return this.entry;
+	}
+
+	public void setEntry(Entry entry) {
+		entry.addComment(this);
+		this.entry = entry;
+	}
 }
--- src/br/com/zamlutti/comente/entities/Entry.java
+++ src/br/com/zamlutti/comente/entities/Entry.java
@@ -6,52 +6,50 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "entries")
 public class Entry {
-    @Id
-    @GeneratedValue
-    private int id;
-    private String title;
-    private String url;
+	@Id
+	@GeneratedValue
+	private int id;
+	private String title;
+	private String url;
 
-    @OneToMany
-    private List<Comment> comments;
+	@OneToMany(mappedBy = "entry")
+	private final List<Comment> comments;
 
-    public Entry() {
-        this.comments = new ArrayList<Comment>();
-    }
+	public Entry() {
+		this.comments = new ArrayList<Comment>();
+	}
 
-    public int getId() {
-        return this.id;
-    }
+	public int getId() {
+		return this.id;
+	}
 
-    public String getTitle() {
-        return this.title;
-    }
+	public String getTitle() {
+		return this.title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getUrl() {
-        return this.url;
-    }
+	public String getUrl() {
+		return this.url;
+	}
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
-    public List<Comment> getComments() {
-        return this.comments;
-    }
+	public List<Comment> getComments() {
+		return this.comments;
+	}
 
-    public void addComment(Comment comment) {
-        this.comments.add(comment);
-    }
+	public void addComment(Comment comment) {
+		this.comments.add(comment);
+	}
 }
